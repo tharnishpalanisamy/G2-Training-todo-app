e95be2e1c1a15e33ebc15c12a36d6f3c342b76eb
Tharnish Palanisamy <tharnishpalanisamy3@gmail.com>
2026-05-19 16:26:48 +0530

    edit task , delete task and complete task has been implemented

--- scripts/script.js
+++ scripts/script.js
@@ -1,156 +1,353 @@
-$(document).ready(function(){
-let name = document.querySelector(".name")
-let user = JSON.parse(localStorage.getItem("user"))
-
-if(!name) {
-    window.location.href = '../index.html'
-}
-name.innerText = `Welcome, ${user.username}` 
-
-async function loadTask(){
-    try{
-        let data = await fetch('http://localhost:3000/todos') 
-        let tasks = await data.json()
-        tasks.filter(task=>task.userId === user.id).forEach(task=>createTask(task))
+    $(document).ready(function(){
+    let name = document.querySelector(".name")
+    let user = JSON.parse(localStorage.getItem("user"))
+    let curPage = 'pending'
+    if(!name) {
+        window.location.href = '../index.html'
     }
-    catch(error){
-        console.error("Failed to load tasks",error)
-    }
-}
-loadTask()
-async function postTask(task) {
+    name.innerText = `Welcome, ${user.username}` 
+
+    async function loadTask(){
         try{
-            let postedData = await fetch('http://localhost:3000/todos' , {
-            method:'POST' , 
+            let data = await fetch('http://localhost:3000/todos') 
+            let tasks = await data.json()
+            let curDate = new Date()
+            tasks.filter(task=>{
+                let dueDate = new Date(task.dueDate)
+                if(curDate > dueDate && task.completed === false){  
+                    task.status = 'overdue'
+                    task.overDue = true 
+                }
+                return task.userId === user.id && task.status === 'pending' })
+                .forEach(task=>createTask(task)
+            
+            )
+        }
+        catch(error){
+            console.error("Failed to load tasks",error)
+        }
+    }
+    loadTask()
+    async function postTask(task) {
+            try{
+                let postedData = await fetch('http://localhost:3000/todos' , {
+                method:'POST' , 
+                headers:{
+                    'content-type':'application/json' 
+                } , 
+                body:JSON.stringify(task)
+                
+                })
+                if(!postedData.ok){
+                    throw new Error("Posting failed")
+                }
+                let result = await postedData.json() 
+                console.log(result);
+            }
+            catch(error) {
+                console.log(error);
+                
+            }
+            
+        }
+    $("#addTaskBtn").on('click',function(){
+
+        if (!$('#task-title').val() || !$('#priority').val() || !$('#due-date').val()) {
+            alert('Please fill in required fields')
+            return
+        }
+
+        const task = {
+            id:Date.now(), 
+            userId:user.id,
+            title:$('#task-title').val(),
+            description:$('#description').val(),
+            priority:$('#priority').val(),
+            dueDate:$('#due-date').val() , 
+            completed:false  , 
+            deleted : false , 
+            overDue:false,
+            status : 'pending' , 
+            createdAt : new Date().toISOString() , 
+            updatedAt: new Date().toISOString()
+        }
+        $('#task-title').val('')
+        $("#description").val('')
+        $('#priority').val('')
+        $('#due-date').val('')
+
+        postTask(task)
+        console.log(task);
+        
+        
+        
+        
+        bootstrap.Modal
+        .getOrCreateInstance(
+            document.getElementById('taskModal')
+        )
+        .hide()
+
+    })
+    //task creation  
+
+    function createTask(task){
+        if (task.deleted === true){
+            return
+        }
+
+        let div = document.createElement("div")
+
+        let stars = ""
+
+        if(task.priority === "High"){
+            stars = "⭐⭐⭐"
+        }
+        else if(task.priority === "Medium"){
+            stars = "⭐⭐"
+        }
+        else{
+            stars = "⭐"
+        }
+
+        let status = task.status 
+        let statusClass
+        if (status=="pending") {
+            statusClass = "btn-warning"
+        }
+        else if (status=="completed") {
+            statusClass = "btn-success"
+        }
+        else if (status=="overdue") {
+            statusClass = "btn-danger"
+        }
+
+
+
+        div.classList.add("task-card")
+
+        let checked = ""
+
+        if(task.completed === true){
+            checked = "checked"
+        }
+        div.innerHTML = `
+            <div class='task-item p-2 '> 
+                <div class='task-header d-flex justify-content-between px-2'> 
+                    <div class = 'task-header-left d-flex gap-4'>
+                        <input type = 'checkbox' class = 'completed-checkbox' data-id = '${task.id}' ${checked}>
+                        <h3 class='fw-bold blue' > ${task.title} </h3> 
+                        <p>${stars} </p> 
+                    </div> 
+                        
+                    <div class = 'task-header-right d-flex gap-2'> 
+                        <button type='button' class = 'btn btn-warning editBtn' data-id='${task.id}' > Edit </button>
+                        <button type='button' class = 'btn btn-danger deleteBtn' data-id='${task.id}'  > Delete </button> 
+                    </div> 
+                </div>
+                
+                <div class = 'task-body d-flex flex-column gap-2'> 
+                    <div class = 'task-body-left d-flex gap-2 align-items-center pt-1' > 
+                        <h5 class = 'fw-bold pt-1' > Due Date : ${task.dueDate} </h5> 
+                        <a class = 'btn ${statusClass} text-light rounded-pill' > ${task.status} </a>
+                    </div> 
+                    <div class = 'task-description' > 
+                        <p class = 'text-secondary' > ${task.description} </p>
+                    </div>
+
+                </div>
+            </div>
+            `
+
+        document.querySelector(".task-container")
+            .appendChild(div)
+    }
+let currentEditTask = null
+document.addEventListener('click', async function(event){
+
+    if(event.target.classList.contains('editBtn')){
+        let taskId = event.target.dataset.id
+
+        let response = await fetch(`http://localhost:3000/todos/${taskId}`)
+        let task = await response.json()
+        currentEditTask = task 
+        $('#edit-id').val(task.id)
+        $('#edit-title').val(task.title)
+        $('#edit-description').val(task.description)
+        $('#edit-priority').val(task.priority)
+        $('#edit-due-date').val(task.dueDate)
+
+        bootstrap.Modal
+        .getOrCreateInstance(document.getElementById('editTaskModal'))
+        .show()
+    }
+})
+
+$('#updateTaskBtn').on('click', async function(){
+    
+
+    let id = $("#edit-id").val()
+    
+    let updatedTask = {
+        id: id,
+        userId: user.id,
+        title: $("#edit-title").val(),
+        description: $("#edit-description").val(),
+        priority: $("#edit-priority").val(),
+        dueDate: $("#edit-due-date").val(),
+        completed: currentEditTask.completed,
+        deleted: currentEditTask.deleted,
+        status: currentEditTask.status,
+        updatedAt: new Date().toISOString(),
+    }
+
+    await fetch(`http://localhost:3000/todos/${id}`,{
+        method:'PUT',
+        headers:{
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify(updatedTask)
+    })
+
+    bootstrap.Modal
+        .getOrCreateInstance(document.getElementById('editTaskModal'))
+        .hide()
+
+    location.reload()
+})  
+
+async function deleteTask(task){
+    task.status = 'delete' 
+    task.deleted = true 
+
+    await fetch(`http://localhost:3000/todos/${task.id}` , {
+        method:'PUT' , 
+        headers:{
+            'Content-type':'application/json' 
+        } , 
+        body:JSON.stringify(task)
+    })
+}
+$(document).on('click',async function(event){
+    if(event.target.classList.contains("deleteBtn")) {
+        let taskId = event.target.dataset.id 
+
+        let response = await fetch(`http://localhost:3000/todos/${taskId}`)  
+
+        let task = await response.json() 
+
+        deleteTask(task) 
+        if (curPage == 'pending') {
+            loadTask() 
+        }
+        else if (curPage == 'completed'){
+            displayCompleteTasks()
+        }
+        else{
+            displayOverDueTasks()
+        }
+
+    }
+})
+
+//checbbox
+
+
+$(document).on('change',async function(event){
+    if(event.target.classList.contains('completed-checkbox')) {
+        let taskId = event.target.dataset.id 
+
+        let response = await fetch(`http://localhost:3000/todos/${taskId}`) 
+        let task = await response.json() 
+
+        let isChecked = event.target.checked 
+        
+        if(isChecked){
+            task.completed = true 
+            task.status = 'completed'
+        }
+        else{
+            task.completed = false 
+
+            let curDate = new Date() 
+            let dueDate = new Date(task.dueDate) 
+
+            if(curDate > dueDate) {
+                task.status = 'overdue' 
+                task.overDue = true 
+            }
+            else{
+                task.status = 'pending' 
+                
+            }
+        }
+
+
+        await fetch(`http://localhost:3000/todos/${taskId}` , {
+            method:"PUT",
             headers:{
-                'content-type':'application/json' 
+                'Content-type':'application/json'
             } , 
             body:JSON.stringify(task)
-            
-            })
-            if(!postedData.ok){
-                throw new Error("Posting failed")
-            }
-            let result = await postedData.json() 
-            console.log(result);
-        }
-        catch(error) {
-            console.log(error);
-            
-        }
+        })
+        location.reload()
         
     }
-$("#addTaskBtn").on('click',function(){
-
-    if (!$('#task-title').val() || !$('#priority').val() || !$('#due-date').val()) {
-        alert('Please fill in required fields')
-        return
-    }
-
-    const task = {
-        id:Date.now(), 
-        userId:user.id,
-        title:$('#task-title').val(),
-        description:$('#description').val(),
-        priority:$('#priority').val(),
-        dueDate:$('#due-date').val() , 
-        completed:false  , 
-        deleted : false , 
-        status : 'pending' , 
-        createdAt : new Date().toISOString() , 
-        updatedAt: new Date().toISOString()
-    }
-    $('#task-title').val('')
-    $("#description").val('')
-    $('#priority').val('')
-    $('#due-date').val('')
-
-    postTask(task)
-    console.log(task);
-    
-    
-    
-    
-    bootstrap.Modal
-    .getOrCreateInstance(
-        document.getElementById('taskModal')
-    )
-    .hide()
-
 })
-//task creation  
+//show pending tasks 
+$("#pending-btn").on('click',async function(){
+    document.querySelector('.task-container').innerHTML = "" 
+    loadTask()
+})
 
-function createTask(task){
-
-    let div = document.createElement("div")
-
-    let stars = ""
-
-    if(task.priority === "High"){
-        stars = "⭐⭐⭐"
+//show completed takss 
+async function displayCompleteTasks(){
+    try{
+            let data = await fetch('http://localhost:3000/todos') 
+            let tasks = await data.json()
+            tasks.filter(task=>task.userId === user.id && task.status === 'completed').forEach(task=>createTask(task))
+        }
+        catch(error){
+            console.error("Failed to load tasks",error)
+        }
     }
-    else if(task.priority === "Medium"){
-        stars = "⭐⭐"
-    }
-    else{
-        stars = "⭐"
-    }
-
-    let status = task.status 
-    let statusClass
-    if (status=="pending") {
-        statusClass = "btn-warning"
-    }
-    else if (status=="completed") {
-        statusClass = "btn-success"
-    }
-    else if (status=="overdue") {
-        statusClass = "btn-danger"
-    }
-
-
-
-    div.classList.add("task-card")
-
-    div.innerHTML = `
-        <div class='task-item p-2 '> 
-            <div class='task-header d-flex justify-content-between px-2'> 
-                <div class = 'task-header-left d-flex gap-4'>
-                    <input type = 'checkbox' class = 'completed-checkbox' id = '${task.taskId}'>
-                    <h3 class='fw-bold blue' > ${task.title} </h3> 
-                    <p>${stars} </p> 
-                </div> 
-                    
-                <div class = 'task-header-right d-flex gap-2'> 
-                    <button type='button' class = 'btn btn-warning editBtn' > Edit </button>
-                    <button type='button' class = 'btn btn-danger' > Delete </button> 
-                </div> 
-            </div>
-            
-            <div class = 'task-body d-flex flex-column gap-2'> 
-                <div class = 'task-body-left d-flex gap-2 align-items-center pt-1' > 
-                    <h5 class = 'fw-bold pt-1' > Due Date : ${task.dueDate} </h5> 
-                    <a class = 'btn ${statusClass} text-light rounded-pill' > ${task.status} </a>
-                </div> 
-                <div class = 'task-description' > 
-                    <p class = 'text-secondary' > ${task.description} </p>
-                </div>
-
-            </div>
-        </div>
-        `
-
-    document.querySelector(".task-container")
-        .appendChild(div)
-}
-document.addEventListener('click',function(event){
-    if(event.target.classList.contains('editBtn')){
-        alert('edit button is working')
-    }
+    
+//completed btn 
+$('#completed-btn').on('click',async function(event){
+    document.querySelector(".task-container").innerHTML = ""
+    await displayCompleteTasks()
 })
 
 
+//overdue
+async function displayOverDueTasks(){
+    try{
+        let response = await fetch('http://localhost:3000/todos') 
+        let tasks = await response.json() 
+        let curDate = new Date()
 
-
+        tasks.forEach(task =>{
+            let dueDate = new Date(task.dueDate)
+            if(curDate > dueDate && task.completed == false){
+                task.status = 'overdue' 
+                task.overDue = true 
+            }
+        })
+        tasks.filter(task=>{
+            return task.userId == user.id && task.status === 'overdue'  
+        }).forEach(task =>{
+            createTask(task)
+        })
+    }
+    catch(error) {
+        console.log(error);
+        
+    }
 }
-)
+
+$("#overdue-btn").on('click',async function(){
+    document.querySelector(".task-container").innerHTML = "" 
+    await displayOverDueTasks()
+})
+
+})
